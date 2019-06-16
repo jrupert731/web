934e43c5f5666ea3ec005c655098a24753a655bd
joeyrupert <joeyrupert731@gmail.com>
2019-06-16 11:56:50 -0400

    Made improvements to web app.

--- inferno/src/App.js
+++ inferno/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {apiResponse: ""};
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <script>
+            function load_home() {
+
+              document.getElementById("content").innerHTML = '<object type="text/html" data="home.html" ></object>'
+
+            }
+          </script>
+          <p className="App-intro">;{this.state.apiResponse}</p>
+        </div>
+    );
+  }
+
 }
 
 export default App;
